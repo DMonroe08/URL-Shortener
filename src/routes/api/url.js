f04f078d7c9178cbc43af949338f5b983d5f3eb0
Danielle Monroe <dmonroe08@fullsail.edu>
2017-05-17 13:02:35 -0400

    makes corrections to style guide specifications

--- src/routes/api/url.js
+++ src/routes/api/url.js
@@ -3,79 +3,56 @@ const url = require('../../models/url');
 module.exports = (express) => {
   const router = express.Router();
 
-//pulls in user input, shortens it, responds with original and new url
-// router.post('/urls', (req, res) => {
-//   var shortURL = require('../shorten');
-//   url.create(req.body, (err) =>{
-//     res.json({url: req.body.url, short: req.body.shortURL.shortURL()});
-//   })
-// });
-
-// router.post('/urls', (req, res) => {
-//   var shortURL = require('../shorten');
-//   url.create({url: req.body.url, short: req.body.shortURL.shortURL()});
-// });
-
-
-router.post('/urls', (req, res) => {
+//  pulls in user input, shortens it, responds with original and new url
+  router.post('/urls', (req, res) => {
+    url.create(req.body, (err) => {
+      req.body.shortURL.shortURL();
+      // const shortURL = require('../shorten');
+      res.json({ url: req.body.url, short: req.body.shortURL.shortURL() });
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
 
 
-  url.create(req.body, (err) => {
-    req.body.shortURL.shortURL();
-    var shortURL = require('../shorten');
-    res.json({url: req.body.url, short: req.body.shortURL.shortURL()});
-    res.status(500).json(err);
-  }, (data) => {
-    res.status(200).json(data);
-  })
-});
-
-
-//gets all urls
+// gets all urls
   router.get('/urls', (req, res) => {
     url.findAll((err) => {
       res.status(500).json(err);
-    })
+    });
   });
 
-  //gets url by specific id
+  // gets url by specific id
   router.get('/urls/:id', (req, res) => {
     req.body.id = req.params.id;
     url.find(req.body, (err) => {
       req.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);
-    })
+    });
   });
 
-//creates creates a url
-// router.post('/urls', (req, res) => {
-//   url.create(req.body, (err) => {
-//     res.status(500).json(err);
-//   }, (data) => {
-//     res.status(200).json(data);
-//   })
-// });
 
-//updates a specific urls information
-router.post('/urls/:id', (req, res) => {
-  req.body.id = req.params.id;
-  url.update(req.body, (err) => {
-    res.status(500).json(err);
-  }, (data) => {
-    res.status(200).json(data);
-  })
-});
+// updates a specific urls information
+  router.post('/urls/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.update(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
 
-//deletes url by id
-router.delete('/urls/:id', (req, res) => {
-  req.body.id = req.params.id;
-  url.destroy  (req.body, (err) => {
-    res.status(500).json(err);
-  }  , (data)=>{
-    res.status(200).json(data);
-  })
-});
+// deletes url by id
+  router.delete('/urls/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.destroy(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
 
-return router;
-}; //Ends module
+  return router;
+}; // Ends module
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 module.exports = (express) => {
   const router = express.Router();
 
-//consoles out working when /url is used in localhost
+  // consoles out working when /url is used in localhost
   router.get('/url', (req, res) => {
     console.log('Router Working', req.body);
-    res.json({working: 'Working'})
+    res.json({ working: 'Working' });
   });
 
-  //creates endpoints
+  // creates endpoints
   router.use('/api/v1', require('./api/url')(express));
 
   return router;
-}
+};
